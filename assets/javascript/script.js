0b8ea264b8db6e0b6475208f1d3a23c1cb9d07a9
elissamaine <emaine97@yahoo.com>
2023-02-26 15:03:21 -0800

    added the date using dayjs to the top of the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var rootEl = $('root')
+//var todaysDateEl = $('currentDay');
+var currentTime = dayjs().format('H');
+
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -19,6 +24,10 @@ $(function () {
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
+
     // TODO: Add code to display the current date in the header of the page.
-  });
-  
+    var todaysDate = dayjs().format('dddd, MMMM D')
+    $('#currentDay').text(todaysDate);
+});
+
+
