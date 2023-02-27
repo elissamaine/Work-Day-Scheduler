d15d8aebbc2915cc6eac6544da7dc9b57d9fc29d
elissamaine <emaine97@yahoo.com>
2023-02-26 16:54:00 -0800

    added code to pull users schedule from local storage and add it to the site schedule

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,10 +33,18 @@ $(function () {
     
     
     
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
+    //code that gets any user input that was saved in localStorage and set sets
+    // it as the text value for each hours description class
+    $('#hour-9 > .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 > .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 > .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 > .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 > .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 > .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 > .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 > .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 > .description').val(localStorage.getItem('hour-17'));
+
 
 
     //code that displays the current date in the header of the page.
